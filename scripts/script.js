8f4e3c30f500917a7aebacce76cf54e4a9279510
KabAyrat <kabirov.ayrat2004@gmail.com>
2024-07-05 13:18:41 +0300

    сделал аудиоплеер прикрепленным к низу экрана

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-function openNav() {  //    ОТКРЫТИЕ/ЗАКРЫТИЕ НАВИГАЦИОННОЙ ПАНЕЛИ
-    document.getElementById("sideNav").style.width = "250px";
+function openNav() {            // ОТКРЫТИЕ/ЗАКРЫТИЕ НАВИГАЦИОННОЙ ПАНЕЛИ
+    document.getElementById("sideNav").style.width = "250px";          
     document.getElementById("main").style.marginLeft = "250px";
 }
 
@@ -8,7 +8,7 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-document.getElementById("themeSwitch").addEventListener("change", function() {      //ПЕРЕКЛЮЧАТЕЛЬ МЕЖДУ ТЕМАМИ
+document.getElementById("themeSwitch").addEventListener("change", function() {      // ПЕРЕКЛЮЧАТЕЛЬ МЕЖДУ ТЕМАМИ
     const body = document.body;
     if (this.checked) {
         body.classList.remove("light-theme");
@@ -18,7 +18,7 @@ document.getElementById("themeSwitch").addEventListener("change", function() {  
         body.classList.add("light-theme");
     }
 });
-function openModal(trackSrc, coverSrc, trackTitle, artist) { // ОТКРТЫИЕ МОДАЛЬНОГО ОКНА
+function openModal(trackSrc, coverSrc, trackTitle, artist) {        // ОТКРЫТИЕ МОДАЛЬНОГО ОКНА
     const modal = $('#trackModal');
     const audio = document.getElementById('modalAudio');
 
@@ -34,3 +34,41 @@ function openModal(trackSrc, coverSrc, trackTitle, artist) { // ОТКРТЫИЕ
 
     modal.modal('show');
 }
+function openPlayer(audioSrc, coverSrc, title, artist) {        // ОТКРЫТИЕ ПЛЕЕРА
+    const playerAudio = document.getElementById('playerAudio');
+    const playerAudioSource = document.getElementById('playerAudioSource');
+    const playerTrackCover = document.getElementById('playerTrackCover');
+    const playerTrackTitle = document.getElementById('playerTrackTitle');
+    const playerTrackArtist = document.getElementById('playerTrackArtist');
+    const fixedPlayer = document.getElementById('fixedPlayer');
+
+    if (playerAudio && playerAudioSource && playerTrackCover && playerTrackTitle && playerTrackArtist && fixedPlayer) {
+        playerAudioSource.src = audioSrc;
+        playerTrackCover.src = coverSrc;
+        playerTrackTitle.textContent = title;
+        playerTrackArtist.textContent = artist;
+        playerAudio.load();
+        fixedPlayer.classList.add('show');
+        playerAudio.play();
+    }
+}
+
+function closePlayer() {    // ЗАКРЫТИЕ ПЛЕЕРА
+    const playerAudio = document.getElementById('playerAudio');
+    const fixedPlayer = document.getElementById('fixedPlayer');
+
+    if (playerAudio && fixedPlayer) {
+        fixedPlayer.classList.remove('show');
+        playerAudio.pause();
+    }
+}
+
+document.getElementById('fixedPlayer').addEventListener('click', function() {       // ОТКРЫТИЕ МОДАЛЬНОГО ОКНА
+    if (window.innerWidth <= 768) {
+        const playerAudioSrc = document.getElementById('playerAudioSource').src;
+        const playerTrackCoverSrc = document.getElementById('playerTrackCover').src;
+        const playerTrackTitle = document.getElementById('playerTrackTitle').textContent;
+        const playerTrackArtist = document.getElementById('playerTrackArtist').textContent;
+        openModal(playerAudioSrc, playerTrackCoverSrc, playerTrackTitle, playerTrackArtist);
+    }
+});
